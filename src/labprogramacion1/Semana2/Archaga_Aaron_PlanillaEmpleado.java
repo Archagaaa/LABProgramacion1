51484f3b4d3ef4effbf41ee3f38af4d31618fe20
Aaron Archaga <Aaron Archaga@Laptop_Archaga>
2024-10-18 08:48:22 -0600

    En este trabajo utilice las variables horasMes, tarifaHora, SalarioSemanal como double ya que son propensas a que los valores sean en decimales. En la primer parte el programa le solicita al usuario que ingrese todos los datos solicitados y una vez ya ingresados. Para calcular el salario semanal se multiplican las horas trabajadas al mes por la tarifa por hora, todo eso se divide entre 4 para poder sacar el salario semanal. Luego solo se presenta otra parte donde muestra todos los datos en forma de lista incluyendo ya su salario semanal calculado.

---
+++ src/labprogramacion1/Semana2/Archaga_Aaron_PlanillaEmpleado.java
@@ -0,0 +1,27 @@
+package labprogramacion1.Semana2;
+
+import java.util.Scanner;
+
+
+public class Archaga_Aaron_PlanillaEmpleado {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        double horasMes, tarifaHora,salarioSemanal;
+        String nombre;
+        
+        System.out.print("Ingrese su nombre y apellido: ");
+        nombre = entrada.nextLine();
+        System.out.print("Ingrese la cantidad de horas laboradas al mes: ");
+        horasMes = entrada.nextDouble();
+        System.out.println("Ingrese la tarifa por hora: ");
+        tarifaHora = entrada.nextDouble();
+        salarioSemanal = (horasMes * tarifaHora)/4;
+        
+        System.out.println("----Boleta Empleado----");
+        System.out.println("Nombre de empleado: "+nombre);
+        System.out.println("Horas de trabajo Mensual: "+horasMes);
+        System.out.println("Tarifa por hora: "+tarifaHora);
+        System.out.println("Salario Semanal: "+salarioSemanal);
+    }
+    
+}
